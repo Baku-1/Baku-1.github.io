adaa9f265b7b5a90960c50a3266f3ecd8f3f99fd
Baku-1 <jeremygordon62716@gmail.com>
2025-03-12 23:49:06 -0500

    Update deploy.js

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,25 +1,27 @@
-const hre = require("hardhat");
+const { ethers, run, network } = require("hardhat");
 
 async function main() {
-  const [deployer] = await hre.ethers.getSigners();
+  await run('compile'); // We are compiling the contracts using Hardhat
+
+  const [deployer] = await ethers.getSigners();
 
   console.log("Deploying contracts with the account:", deployer.address);
   console.log("Account balance:", (await deployer.getBalance()).toString());
 
   // Step 1: Deploy RoninP2PFeeAndRewardDistribution
-  const RoninP2PFeeAndRewardDistribution = await hre.ethers.getContractFactory("RoninP2PFeeAndRewardDistribution");
+  const RoninP2PFeeAndRewardDistribution = await ethers.getContractFactory("RoninP2PFeeAndRewardDistribution");
   const roninP2PFeeAndRewardDistribution = await RoninP2PFeeAndRewardDistribution.deploy(deployer.address, deployer.address);
   await roninP2PFeeAndRewardDistribution.deployed();
   console.log("RoninP2PFeeAndRewardDistribution address:", roninP2PFeeAndRewardDistribution.address);
 
   // Step 2: Deploy RoninP2PTradeAgreement
-  const RoninP2PTradeAgreement = await hre.ethers.getContractFactory("RoninP2PTradeAgreement");
+  const RoninP2PTradeAgreement = await ethers.getContractFactory("RoninP2PTradeAgreement");
   const roninP2PTradeAgreement = await RoninP2PTradeAgreement.deploy(roninP2PFeeAndRewardDistribution.address, roninP2PFeeAndRewardDistribution.address);
   await roninP2PTradeAgreement.deployed();
   console.log("RoninP2PTradeAgreement address:", roninP2PTradeAgreement.address);
 
   // Step 3: Deploy RoninP2POffer
-  const RoninP2POffer = await hre.ethers.getContractFactory("RoninP2POffer");
+  const RoninP2POffer = await ethers.getContractFactory("RoninP2POffer");
   const roninP2POffer = await RoninP2POffer.deploy();
   await roninP2POffer.deployed();
   console.log("RoninP2POffer address:", roninP2POffer.address);
