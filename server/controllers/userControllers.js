ad06dff71fbc970f44998cee5f18e7142bb70c03
Baku-1 <jeremygordon62716@gmail.com>
2025-03-14 11:35:03 -0500

    Create userControllers.js

---
+++ server/controllers/userControllers.js
@@ -0,0 +1,24 @@
+const UserService = require('../services/userService');
+
+const createUser = async (req, res) => {
+  try {
+    const user = await UserService.createUser(req.body);
+    res.status(201).json(user);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
+
+const getUserById = async (req, res) => {
+  try {
+    const user = await UserService.getUserById(req.params.id);
+    res.status(200).json(user);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
+
+module.exports = {
+  createUser,
+  getUserById
+};
