cb270d0610c1a0c3aeee8ff2df7730008193544c
Baku-1 <jeremygordon62716@gmail.com>
2025-03-14 11:51:51 -0500

    Create NFTPage.js

---
+++ client/src/pages/NFTPage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import NFTForm from '../components/NFTForm';
+
+const NFTPage = () => {
+  return (
+    <div>
+      <h1>Create NFT</h1>
+      <NFTForm />
+    </div>
+  );
+};
+
+export default NFTPage;
