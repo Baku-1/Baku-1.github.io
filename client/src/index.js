1a230d553dcc52dbf5f02425e67127fa7507f375
Baku-1 <jeremygordon62716@gmail.com>
2025-03-14 11:56:58 -0500

    Update index.js

--- client/src/index.js
+++ client/src/index.js
@@ -4,10 +4,12 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import store from './store/store';
 import App from './App';
+import './index.css';
 
 ReactDOM.render(
+  <React.StrictMode>
   <Provider store={store}>
     <App />
-  </Provider>,
+  <React.StrictMode>,
   document.getElementById('root')
 );
