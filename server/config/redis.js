a0b04d4282859101e3456ebe0dc559a903cb1cde
Baku-1 <jeremygordon62716@gmail.com>
2025-03-14 12:22:32 -0500

    Update redis.js

--- server/config/redis.js
+++ server/config/redis.js
@@ -2,16 +2,18 @@ const redis = require('redis');
 
 const redisClient = redis.createClient({
   url: process.env.REDIS_URL,
-  });
+});
 
-  redisClient.on('error', (err) => {
-    console.error('Redis error:', err);
-    });
+redisClient.on('error', (err) => {
+  console.error('Redis error:', err);
+});
 
-    redisClient.on('connect', () => {
-      console.log('Connected to Redis');
-      });
+redisClient.on('connect', () => {
+  console.log('Connected to Redis');
+});
 
-      redisClient.connect();
+redisClient.connect().catch((err) => {
+  console.error('Failed to connect to Redis:', err);
+});
 
-      module.exports = redisClient;
+module.exports = redisClient;
