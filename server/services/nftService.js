ef28d5217b0b1edac7db72cc44718bcaa41cc4e3
Baku-1 <jeremygordon62716@gmail.com>
2025-03-14 12:27:58 -0500

    Update nftService.js

--- server/services/nftService.js
+++ server/services/nftService.js
@@ -1,26 +1,12 @@
 const NFT = require('../models/NFT');
 
 class NFTService {
-  async createNFT(data) {
-    const nft = new NFT(data);
-    await nft.save();
-    return nft;
-  }
-
   async getNFTById(id) {
     return NFT.findById(id);
   }
 
   async getAllNFTs() {
     return NFT.find();
-  }
-
-  async updateNFT(id, data) {
-    return NFT.findByIdAndUpdate(id, data, { new: true });
-  }
-
-  async deleteNFT(id) {
-    return NFT.findByIdAndDelete(id);
   }
 }
 
