4e444c060ad66754ecffab0803b1d9415c5a1886
Baku-1 <jeremygordon62716@gmail.com>
2025-03-14 17:54:54 -0500

    Update UserPage.js
    Signed-off-by: Baku-1 <jeremygordon62716@gmail.com>

--- client/src/pages/UserPage.js
+++ client/src/pages/UserPage.js
@@ -6,6 +6,10 @@ const UserPage = () => {
     <div>
       <h1>Register User</h1>
       <UserForm />
+      <button className="button" onClick={() => console.log('Sign In')}>Sign In</button>
+      <button className="button" onClick={() => console.log('Create Trade')}>Create Trade</button>
+      <button className="button" onClick={() => window.location.href = '/trades'}>View Trades</button>
+      <button className="button" onClick={() => console.log('Interact with Trades')}>Interact with Trades</button>
     </div>
   );
 };
