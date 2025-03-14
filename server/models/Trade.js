5f94f21137c6c1ff62de3aa750b70bf343a8cf3a
Baku-1 <jeremygordon62716@gmail.com>
2025-03-14 12:21:33 -0500

    Update Trade.js

--- server/models/Trade.js
+++ server/models/Trade.js
@@ -1,23 +1,48 @@
 const mongoose = require('mongoose');
+const { sanitize } = require('express-validator'); // Added for sanitization
 
 const TradeSchema = new mongoose.Schema({
   user1: {
     type: mongoose.Schema.Types.ObjectId,
     ref: 'User',
     required: true,
+    validate: {
+      validator: function(v) {
+        return mongoose.Types.ObjectId.isValid(v);
+      },
+      message: props => `${props.value} is not a valid user id`
+    },
   },
   user2: {
     type: mongoose.Schema.Types.ObjectId,
     ref: 'User',
     required: true,
+    validate: {
+      validator: function(v) {
+        return mongoose.Types.ObjectId.isValid(v);
+      },
+      message: props => `${props.value} is not a valid user id`
+    },
   },
   nftIds: {
     type: [String],
     default: [],
+    validate: {
+      validator: function(v) {
+        return v.length <= 5;
+      },
+      message: props => `You can only trade up to 5 NFTs or tokens`
+    },
   },
   tokenIds: {
     type: [String],
     default: [],
+    validate: {
+      validator: function(v) {
+        return v.length <= 5;
+      },
+      message: props => `You can only trade up to 5 NFTs or tokens`
+    },
   },
   status: {
     type: String,
@@ -27,6 +52,7 @@ const TradeSchema = new mongoose.Schema({
   attempts: {
     type: Number,
     default: 0,
+    max: [3, 'Modification limit reached'],
   },
   createdAt: {
     type: Date,
