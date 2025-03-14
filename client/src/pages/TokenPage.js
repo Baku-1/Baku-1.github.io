d26daa7a41229c45e7b0362473ecdd24a8597210
Baku-1 <jeremygordon62716@gmail.com>
2025-03-14 11:52:32 -0500

    Create TokenPage.js

---
+++ client/src/pages/TokenPage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import TokenForm from '../components/TokenForm';
+
+const TokenPage = () => {
+  return (
+    <div>
+      <h1>Create Token</h1>
+      <TokenForm />
+    </div>
+  );
+};
+
+export default TokenPage;
