cff78659838ced1ec98005a070abc101f094b61f
Baku-1 <jeremygordon62716@gmail.com>
2025-03-14 17:49:45 -0500

    Update AdministrationDashboard.js
    Signed-off-by: Baku-1 <jeremygordon62716@gmail.com>

--- client/src/pages/AdministrationDashboard.js
+++ client/src/pages/AdministrationDashboard.js
@@ -4,15 +4,18 @@ import Footer from '../components/Footer';
 
 const AdminDashboard = () => {
   return (
-      <div>
-            <Header />
-                  <main className="container">
-                          <h1 className="my-4">Admin Dashboard</h1>
-                                  <p>Manage the platform and view transaction statistics.</p>
-                                        </main>
-                                              <Footer />
-                                                  </div>
-                                                    );
-                                                    };
+    <div>
+      <Header />
+      <main className="container">
+        <h1 className="my-4">Admin Dashboard</h1>
+        <p>Manage the platform and view transaction statistics.</p>
+        <button className="button" onClick={() => console.log('Sign In')}>Sign In</button>
+        <button className="button" onClick={() => console.log('Create Trade')}>Create Trade</button>
+        <button className="button" onClick={() => console.log('Disburse Rewards')}>Disburse Rewards</button>
+      </main>
+      <Footer />
+    </div>
+  );
+};
 
-                                                    export default AdminDashboard;
+export default AdminDashboard;
