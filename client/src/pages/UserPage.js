e0a14d546078ee462bda0086f0d5fc9f4dfdf6c1
Baku-1 <jeremygordon62716@gmail.com>
2025-03-14 11:50:55 -0500

    Create UserPage.js

---
+++ client/src/pages/UserPage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import UserForm from '../components/UserForm';
+
+const UserPage = () => {
+  return (
+    <div>
+      <h1>Register User</h1>
+      <UserForm />
+    </div>
+  );
+};
+
+export default UserPage;
