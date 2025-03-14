5af1fda79aee5fe3fd08213cc0ef590c3c7d7d4e
Baku-1 <jeremygordon62716@gmail.com>
2025-03-14 11:24:01 -0500

    Create NFT.js

---
+++ server/models/NFT.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+
+const NFTSchema = new mongoose.Schema({
+  tokenId: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  owner: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true,
+  },
+  metadata: {
+    type: String,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = mongoose.model('NFT', NFTSchema);
