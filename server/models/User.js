1e99e895bcf3930c35283222ecad2abbc07624ec
Baku-1 <jeremygordon62716@gmail.com>
2025-03-14 11:38:24 -0500

    Update User.js

--- server/models/User.js
+++ server/models/User.js
@@ -1,8 +1,20 @@
 const mongoose = require('mongoose');
 
-const userSchema = new mongoose.Schema({
-  email: { type: String, required: true, unique: true },
-    password: { type: String, required: true }
-    });
+const UserSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  walletAddress: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
 
-    module.exports = mongoose.model('User', userSchema);
+module.exports = mongoose.model('User', UserSchema);
