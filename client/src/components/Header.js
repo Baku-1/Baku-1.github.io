6af1b5454fd8311b93b729181805098b9c6c64af
Baku-1 <jeremygordon62716@gmail.com>
2025-03-14 17:31:30 -0500

    Update Header.js
    Signed-off-by: Baku-1 <jeremygordon62716@gmail.com>

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -2,10 +2,18 @@ import React from 'react';
 
 const Header = () => {
   return (
-      <header className="navbar navbar-dark bg-dark">
-            <a className="navbar-brand" href="/">Ronin P2P Trading Platform</a>
-                </header>
-                  );
-                  };
+    <header>
+      <h1>Ronin P2P Trading Platform</h1>
+      <nav>
+        <ul>
+          <li><a href="/">Home</a></li>
+          <li><a href="/about">About</a></li>
+          <li><a href="/contact">Contact</a></li>
+          <li><a href="/trades">Trades</a></li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
 
-                  export default Header;
+export default Header;
