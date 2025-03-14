176ccdfd9fefe0d61f803ec6e485198cd7b52bbb
Baku-1 <jeremygordon62716@gmail.com>
2025-03-14 18:55:52 -0500

    Update index.js
    Signed-off-by: Baku-1 <jeremygordon62716@gmail.com>

--- client/src/index.js
+++ client/src/index.js
@@ -2,13 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import store from './store/store';
-import App from './App';
+import RouterConfig from './RouterConfig';
 import './index.css';
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <RouterConfig />
     </Provider>
   </React.StrictMode>,
   document.getElementById('root')
