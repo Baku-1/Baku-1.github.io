67fe534fc9def31f831a27432acb0ede60f45830
Baku-1 <jeremygordon62716@gmail.com>
2025-03-14 11:24:39 -0500

    Create Token.js

---
+++ server/models/Token.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+
+const TokenSchema = new mongoose.Schema({
+  tokenAddress: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  owner: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true,
+  },
+  balance: {
+    type: Number,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = mongoose.model('Token', TokenSchema);
