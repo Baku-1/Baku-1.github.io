8c6c3fc65c2640572192a2619010531adc6048f2
Baku-1 <jeremygordon62716@gmail.com>
2025-03-15 13:38:58 -0500

    Update HomePage.js
    Signed-off-by: Baku-1 <jeremygordon62716@gmail.com>

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -5,10 +5,12 @@ import '../../assets/styles/UsersDashboard.css';
 import '../../assets/styles/AdminDashboard.css';
 import '../../index.css'; // Ensure the styles are correctly imported
 
-const latestTrades = [
-    { id: 1, title: 'Completed Trade 1', description: 'Details about completed trade 1' },
-    { id: 2, title: 'Completed Trade 2', description: 'Details about completed trade 2' },
-    // Add more completed trade objects here
+const mockTrades = [
+    { id: 1, title: 'Trade 1', description: 'Details about trade 1: 3 NFTs, 2 Tokens' },
+    { id: 2, title: 'Trade 2', description: 'Details about trade 2: 2 NFTs, 3 Tokens' },
+    { id: 3, title: 'Trade 3', description: 'Details about trade 3: 1 NFT, 4 Tokens' },
+    { id: 4, title: 'Trade 4', description: 'Details about trade 4: 5 NFTs' },
+    { id: 5, title: 'Trade 5', description: 'Details about trade 5: 4 NFTs, 1 Token' },
 ];
 
 const HomePage = () => {
@@ -22,7 +24,7 @@ const HomePage = () => {
         <div className="homepage">
             <header className="navbar">
                 <h1>Welcome to Ronin P2P Trading</h1>
-                <nav>
+                <nav className="button-container">
                     <button className="btn-trade" onClick={() => navigateTo('/signin')}>Sign In</button>
                     <button className="btn-trade" onClick={() => navigateTo('/trades')}>Trade Page</button>
                     <button className="btn-trade" onClick={() => navigateTo('/about')}>About</button>
@@ -36,7 +38,7 @@ const HomePage = () => {
             <div className="container centered-container">
                 <h2>Latest Trades Completed</h2>
                 <div className="grid-view">
-                    {latestTrades.map(trade => (
+                    {mockTrades.map(trade => (
                         <div className="trade-card grid-item" key={trade.id}>
                             <h3>{trade.title}</h3>
                             <p>{trade.description}</p>
