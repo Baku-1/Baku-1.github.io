b201f2bcca99bc4682cd2cd295f5acb18902c213
Baku-1 <jeremygordon62716@gmail.com>
2025-03-14 21:57:40 -0500

    Update UserPage.js
    Signed-off-by: Baku-1 <jeremygordon62716@gmail.com>

--- client/src/pages/UserPage.js
+++ client/src/pages/UserPage.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import UserForm from '../components/UserForm';
+import '../assets/styles/UserPage.css'; // Ensure this path is correct
 
 const UserPage = () => {
   return (
     <div>
       <h1>Register User</h1>
       <UserForm />
-      <button className="button" onClick={() => console.log('Sign In')}>Sign In</button>
-      <button className="button" onClick={() => console.log('Create Trade')}>Create Trade</button>
-      <button className="button" onClick={() => window.location.href = '/trades'}>View Trades</button>
-      <button className="button" onClick={() => console.log('Interact with Trades')}>Interact with Trades</button>
+      <div className="button-group">
+        <button className="button" onClick={() => console.log('Sign In')}>Sign In</button>
+        <button className="button" onClick={() => console.log('Create Trade')}>Create Trade</button>
+        <button className="button" onClick={() => window.location.href = '/trades'}>View Trades</button>
+        <button className="button" onClick={() => console.log('Interact with Trades')}>Interact with Trades</button>
+      </div>
     </div>
   );
 };
