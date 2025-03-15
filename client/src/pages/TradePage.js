8cb374e04d4a6f71d0b5f75da2a69b0163f3e8f2
Baku-1 <jeremygordon62716@gmail.com>
2025-03-15 00:37:12 -0500

    Update TradePage.js
    Signed-off-by: Baku-1 <jeremygordon62716@gmail.com>

--- client/src/pages/TradePage.js
+++ client/src/pages/TradePage.js
@@ -7,41 +7,6 @@ import TradeCard from '../components/TradeCard';
 const TradePage = () => {
   const [trades, setTrades] = useState([]);
 
-    useEffect(() => {
-        const fetchTrades = async () => {
-              const response = await axios.get('/api/trades');
-                    setTrades(response.data);
-                        };
-
-                            fetchTrades();
-                              }, []);
-
-                                return (
-                                    <div>
-                                          <Header />
-                                                <main className="container">
-                                                        <h1 className="my-4">Trade Agreements</h1>
-                                                                <div className="row">
-                                                                          {trades.map((trade) => (
-                                                                                      <TradeCard key={trade._id} trade={trade} />
-                                                                                                ))}
-                                                                                                        </div>
-                                                                                                              </main>
-                                                                                                                    <Footer />
-                                                                                                                        </div>
-                                                                                                                          );
-                                                                                                                          };
-
-                                                                                                                          export default TradePage;
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
-import Header from '../components/Header';
-import Footer from '../components/Footer';
-import TradeCard from '../components/TradeCard';
-
-const TradePage = () => {
-  const [trades, setTrades] = useState([]);
-
   useEffect(() => {
     const fetchTrades = async () => {
       const response = await axios.get('/api/trades');
@@ -61,9 +26,9 @@ const TradePage = () => {
     console.log("Modify Trade button clicked");
   };
 
-  const handleSelectTrades = () => {
-    // Logic for selecting trades
-    console.log("Select Trades button clicked");
+  const handleViewTrades = () => {
+    // Logic for viewing trades
+    console.log("View Trades button clicked");
   };
 
   return (
@@ -74,7 +39,7 @@ const TradePage = () => {
         <div className="button-container">
           <button className="btn-trade" onClick={handleCreateTrade}>Create Trade</button>
           <button className="btn-trade" onClick={handleModifyTrade}>Modify Trade</button>
-          <button className="btn-trade" onClick={handleSelectTrades}>Select Trades Available</button>
+          <button className="btn-trade" onClick={handleViewTrades}>View Trades</button>
         </div>
         <div className="row">
           {trades.map((trade) => (
